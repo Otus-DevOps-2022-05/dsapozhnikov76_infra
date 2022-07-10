ea58b40d928979779a449eee874d9bedb771d616
Дмитрий Сапожников <dsapozhnikov76@gmail.com>
2022-07-10 16:38:49 +0300

    Terraform - 2 homework init

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-/*
 terraform {
    required_providers {
      yandex = {
@@ -6,7 +5,6 @@ terraform {
      }
    }
 }
-*/
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
@@ -16,50 +14,16 @@ provider "yandex" {
 }
 
 
-resource "yandex_compute_instance" "app" {
-  name = "reddit-app-${count.index}"
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+}
 
-  count = 2
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
-      image_id = var.image_id
-    }
-  }
-
-  network_interface {
-    # Указан id подсети default-ru-central1-a
-    subnet_id = var.subnet_id
-
-    nat = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-
-  connection {
-    type  = "ssh"
-    host  = self.network_interface.0.nat_ip_address
-    user  = "ubuntu"
-    agent = false
-    # путь до приватного ключа
-    private_key = file(var.private_key)
-  }
-
+module "db" {
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,6 @@
 output "external_ip_address_app" {
-  value = yandex_compute_instance.app[*].network_interface.0.nat_ip_address
+  value = module.app.external_ip_address_app
 }
-
-output "external_ip_address_lb" {
-  value = yandex_lb_network_load_balancer.reddit-app-nlb.listener.*.external_address_spec[0].*.address
+output "external_ip_address_db" {
+  value = module.db.external_ip_address_db
 }
